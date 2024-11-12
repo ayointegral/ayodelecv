715ddab09ecf01211aa8df203517b553c64511c5
Ayo <ayointegral@users.noreply.github.com>
2024-11-12 09:42:01 +0000

    reset

--- next.config.ts
+++ next.config.ts
@@ -1,46 +1,8 @@
 import type { NextConfig } from "next";
-import TerserPlugin from "terser-webpack-plugin";
 
 const nextConfig: NextConfig = {
+  /* config options here */
   reactStrictMode: true,
-  distDir: 'build', // specify the output directory for Cloudflare Pages
-
-  // Optimize the build size
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // Use Terser to minimize JS files
-      config.optimization = {
-        ...config.optimization,
-        minimize: true,
-        minimizer: [
-          new TerserPlugin({
-            terserOptions: {
-              compress: {
-                drop_console: true, // Remove console logs
-                drop_debugger: true, // Remove debugger statements
-              },
-              output: {
-                comments: false, // Remove comments
-              },
-            },
-          }),
-        ],
-      };
-    }
-
-    return config;
-  },
-
-  // Enable experimental features
-  experimental: {},
-
-  // Future options to improve bundle size with granular chunks
-  modularizeImports: {
-    // Example: if you use lodash or other large libraries, load only what’s needed
-    lodash: {
-      transform: 'lodash/{{member}}',
-    },
-  },
 };
 
 export default nextConfig;
