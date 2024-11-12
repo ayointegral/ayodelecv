edbcaa1495121929080b4788f0e1d577ab9b1fa9
Ayo <ayointegral@users.noreply.github.com>
2024-11-12 15:33:46 +0000

    update package.json and package-lock.json

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 // pages/_app.tsx
 import { ThemeProvider } from 'next-themes'
-import type { AppProps } from 'next/app'
+import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import '../styles/globals.css'
+import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
       <Component {...pageProps} />
+      <SpeedInsights />
+      <Analytics />
     </ThemeProvider>
   )
 }
